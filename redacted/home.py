bb4c1323025df646e4627033a0d19f262087a85c
Viktor <viktormg.blesk@gmail.com>
2020-11-01 16:29:46 +0100

    You can now sleep until morning, and when you make noods you get a confirmation message

--- redacted/home.py
+++ redacted/home.py
@@ -7,14 +7,28 @@ class Bedroom(game.Location):
     def __init__(self):
         super().__init__()
 
-        @game.object("bed")
+        @self.object("bed")
         def bed():
             pass
 
-        @bed.action(name="sleep", description="Take a 8 hour nap",  time_cost=datetime.timedelta(hours=8))
-        def sleep():
+        @bed.action(name="Nap", description="Take a 8 hour nap", time_cost=datetime.timedelta(hours=8))
+        def nap():
             game.show_message("You took a nice nap")
-            utils.sleep()
+            #utils.sleep()
+
+        @bed.action(name="Sleep", description="Sleep until 6 in the morning", time_cost=datetime.timedelta(0))
+        def sleep():
+            if game.game_state.time.hour > 11:
+                game.game_state.time = game.game_state.time + datetime.timedelta(days=1)
+                game.game_state.time = game.game_state.time.replace(hour=7, minute=0, second=0)
+                game.show_message("You slept through the night")
+                #utils.sleep()
+            elif game.game_state.time.hour < 7:
+                game.game_state.time = game.game_state.time.replace(hour=7, minute=0, second=0)
+                game.show_message("You slept until the morning")
+                #utils.sleep()
+            else:
+                game.show_message("It is no time to sleep")
 
         @self.object("kettle")
         def kettle():
@@ -22,8 +36,8 @@ class Bedroom(game.Location):
 
         @kettle.action(name="Make noodles", time_cost=datetime.timedelta(minutes=5))
         def make_noodles():
-            utils.eat("noodles")
-
+            game.show_message("you are eaten instant noods\ncongratulates")
+            #utils.eat("noodles")
 
 class Hallway(game.Location):
     def __init__(self):
@@ -37,13 +51,13 @@ class Hallway(game.Location):
 
 
 def init():
-    global bed, hall
-    bed = Bedroom()
+    global bedroom, hall
+    bedroom = Bedroom()
     hall = Hallway()
-    bed.add_neighbor(hall)
+    bedroom.add_neighbor(hall)
 
 
 init()
 def callback():
     pass
-game.game_init(bed, callback)
+game.game_init(bedroom, callback)
