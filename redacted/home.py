f8a2f4c88a8a47c6420480adef86ab573c26b757
Havoš <matej.haverlik@gmail.com>
2020-11-02 16:26:48 +0100

    random veci

--- redacted/home.py
+++ redacted/home.py
@@ -22,12 +22,12 @@ class Bedroom(game.Location):
             if time.hour > 18:
                 game.game_state.time = game.game_state.time + datetime.timedelta(days=1)
                 game.game_state.time = game.game_state.time.replace(hour=7, minute=0, second=0)
-                game.show_message("You slept through the night")
+                game.show_message("You slept through the night.")
             elif time.hour < 7:
                 game.game_state.time = game.game_state.time.replace(hour=7, minute=0, second=0)
-                game.show_message("You slept until the morning")
+                game.show_message("You slept until the morning.")
             else:
-                game.show_message("It is no time to sleep")
+                game.show_message("It is no time to sleep right now.")
             utils.sleep(game.game_state.time - time)
 
         @self.object("kettle")
@@ -38,14 +38,14 @@ class Bedroom(game.Location):
         def make_instant_noodles():
             utils.eat(self.instant_noodles)
             utils.remove_from_inventory(self.instant_noodles.name)
-            game.show_message("You cook some Instant noodles and eat them. It ain't much, but it's honest work.")
+            game.show_message("You cook some Instant noodles and eat them. The flavoring is a little bit off.")
             self.check_cookable()
 
         @kettle.action(name="Make Instant soup", time_cost=datetime.timedelta(minutes=5), energycost=game.EnergyCost.NONE, disabled=True)
         def make_instant_soup():
             utils.eat(self.instant_soup)
             utils.remove_from_inventory(self.instant_soup.name)
-            game.show_message("You cook a cup of Instant soup and eat it. It doesn't taste amazing, but at least it's hot.")
+            game.show_message("You cook a cup of Instant soup. It doesn't taste amazing, but at least it's hot.")
             self.check_cookable()
 
     def check_cookable(self):
