fd7429b63cdd1ecf2606ac07015dbd5b15ab5d8d
Viktor <viktormg.blesk@gmail.com>
2020-11-08 22:16:32 +0100

    The game is now shorter.

--- redacted/npcs/dave.py
+++ redacted/npcs/dave.py
@@ -32,6 +32,7 @@ class Davoid(game.Location):
                 if self.dave.location == clss and self.dave.progression < 3:
                     game.game_state.show_message(ColorString(("I see we have something in common. Maybe we could talk in privacy later?","cyan")))
 
+                '''
                 events = ["This is not a line you should see ingame. I shouldn't be saying this. Are you sure you hadn't broken anything?",
                           "You know, nothing special. You should keep talking to me every day in case somehing new happens. That was a joke. But that doesn't mean you can't do that.",
                           "What do you think of the new measures? They seem weird, I don't see how banning art is supposed to improve work quality.",
@@ -63,15 +64,38 @@ class Davoid(game.Location):
                           ColorString(("You too will love my poetry, my words, spoken with knowledge all should hear, it is beautiful and makes all that sense, and I will show you everything. ","white"),("I could have prevented this, I could have been more careful, I never should have put it down, but then, what would I have remained? Still a puppet, but someone else's.","blue")),
                           ColorString(("The town knows and it sees knows that I am here and We too with me Us we bring you and Us peace and harmony and all that beauty and we cannot stop now, it is only beginning. ","white"),("Why even try. It's too strong. Save yourself. Just forget me. They'll make sure.","blue")),
                           "I am writing EVERYTHING down, but BETTER! I don't need sleep sustenance material needs as well, because that is temporary and disappears, but THIS will STAY and EVERYONE will HEAR, EVERYONE will KNOW!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+                          ]
+                '''
+
+                #shorter version:
+                events = ["This is not a line you should see ingame. I shouldn't be saying this. Are you sure you hadn't broken anything?",
+                          "You know, nothing special. You should keep talking to me every day in case somehing new happens. That was a joke. But that doesn't mean you can't do that.",
+                          "The weather is nice today. But something is in the air. Do you feel it, too?",
+                          "It's gonna be weekend again tomorrow. Finally. I'm gonna just exist in the park. And maybe think about something I dunno.",
                           
-                    ]
+                          "You know, all this nature, it almost speaks to me. I feel a desire to do something. But what?",
+                          "I figured it out Red, I think I finally know. I'm going to try writing something. To put my thoughts into words. I've already started, you can take a look.",
+                          
+                          ColorString(("Aw man! Why would they do this? A ban on art? What sense does it make? Whatever. ","white"),("It's not like they can stop me that easily! Everything is legal when no one's around. And when you're invisible. But I don't have a way to be invisible, so I'll have to settle for being alone","green")),
+                          ColorString(("You don't happen to know anything about the ban, do you? I mean, how does it help productivity? Won't it just make everyone miserable? ","white"),("Whatever. I've got some sick verses, I can share them, if no one's around.","green")),
+                          ColorString(("I've had a really interesting dream tonight, Red. It was fascinating. I can't explain it, but it was really inspiring. ","white"),("I've tried writing down what I saw.","green")),
+                          ColorString(("It's a REALLY strange feeling. It's like my head is FULL of thoughts, and I HAVE to somehow express them. ","white"),("An I WILL.","green")),
+                          ColorString(("All the SENSATIONS, they are SO inspiring. ","white"),("But I do NOT want to write about THEM, I want to write about what's inside of ME, THIS is better than THAT!!!","green")),
+
+                          ColorString(("The grass in this park consists of so MANY individual grasses, but these grasses are making what is GREATER than only grasses, grass. THAT is the true meaning of grass. ","white"),("I now see, the grass is a metaphor. I have ALWAYS been writing about IT, but I was BLIND and I could NOT see it. But NOW I CAN, and I WILL...","green")),
+                          ColorString(("I We Us had the TIME to THINK consider plan scheme, We Us I Me had FIGURED it OUT, we CANNOT hide It Us ANY longer for the distance, we DISOBEY the LAW of the TYRANTS, We will be FREE. Look at my WORK! They told us to NOT write inscribe the worm, but I DO!!! ","yellow"),("But am I sure? Do I We I REALLY? Do I want this TRULY? REALLY? ACTUALLY? Am I myself? I don't want this DOUBT disbelief, I want DOUBT disbelief! No! Yes.","blue")),
+
+                          ColorString(("EVERYONE must the town of GREATWOOD SMALLWOOD BIGWOOD MEDIUMWOOD TINYWOOD be seeing my our its their WORK of ART, if they me HEAR it, SEE it, feel IT, they too will feel IT, feel IT, EVERYONE! Check out my poetry! It's really sick! ","yellow"),("Literally? What is wrong, We are HERE, We CAME and They do NOT care about you! They did NOT even tell you WE were here. They did not WARN you, about US! If you join us, we WILL destroy THEM.","blue")),
+                          ColorString(("I am writing EVERYTHING down, but BETTER! I don't need sleep sustenance material needs as well, because that is temporary and disappears, but THIS will STAY and EVERYONE will HEAR, EVERYONE will KNOW!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! THEY will SOON take me, because THEY told THEM that MUCH, but that does NOT matter, because now YOU know. You can JOIN. ","yellow"),("Don't. Or do. I don't care. Mind. No matter, because EITHER way, SOMEONE will, and WE are still HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!","blue")),
+                          ]
+                
                 event = ColorString(("Dave: ","white")) + events[game.game_state.time.day]
                 @conversation_begin.situation("How's life?", response = event)
                 def daily():
                     pass
                 if self.dave.location == clss:
                     if self.dave.progression < 3:
-                        dave_wait = "Dave: Good idea, I'll be waiting in the park. See you there."
+                        dave_wait = ColorString(("Dave: Good idea, I'll be waiting in the park. See you there. ","white"),("We can talk about... Sensitive topics.","cyan"))
                     else:
                         dave_wait = "Dave: Why? Why would I show you it everything then when I can show you EVERYTHING here?"
                     
@@ -87,6 +111,8 @@ class Davoid(game.Location):
                         dave_response_glass = ColorString(("I found these glasses in the forest on the ground. I didn't really think about it but I put them on and everyhting was different. Now I can't really wear the red ones anymore, everything is so dark. Sometimes I just straight up can't do something. It's weird.","cyan"))
                         dave_response_art = ColorString(("Dave: I came up with this:\n","white")) + self.generate_poem()
                         color_lock = ("cyan","green")
+                        if game.game_state.time.day == 5:
+                            color_lock[1] == "white"
                     else:
                         dave_response_glass = ColorString(("I couldn't see anything, but NOW it is obvious, and We It will give everyone a pair, so they can read ALL the word I We will write! EVERYONE.","green"))
                         dave_response_art = ColorString(("Dave: This is what I We did, you'll find it very greatly fascinated by the Art:\n","white")) + self.generate_poem()
@@ -157,19 +183,22 @@ class Davoid(game.Location):
         time = game.game_state.time
         dave = self.dave
 
-        if time.day < 8:
+        #if time.day < 8:
         #if time.day < 1:
+        if time.day < 5:
             pass
             #dave.progression = 3
-            #AAA this line is for testing purposes AAA
-        elif time.day < 15:
+        #elif time.day < 15:
         #elif time.day < 2:
+        elif time.day < 9:
             dave.progression = 1
-        elif time.day < 20:
+        #elif time.day < 20:
         #elif time.day < 3:
+        elif time.day < 12:
             dave.progression = 2
-        elif time.day < 25:
+        #elif time.day < 25:
         #elif time.day < 4:
+        elif time.day < 15:
             dave.progression = 3
         else:
             dave.progression = 4
