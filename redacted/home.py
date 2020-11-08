3b80a764b2bd41e74b1fb590733f5f0f6aa6888a
Viktor <viktormg.blesk@gmail.com>
2020-11-08 18:37:22 +0100

    There is now a magic glass which allows you to cheat the game and just get fake glasses instantly.

--- redacted/home.py
+++ redacted/home.py
@@ -9,6 +9,10 @@ class Bedroom(game.Location):
 
         self.has_lens = True
 
+        @self.action(name="magic glass", description="do them agic glasss")
+        def glass():
+            game.game_state.glasses.type = game.Alignment.INDEPENDENT
+
         @self.object("flag")
         def flag():
             pass
